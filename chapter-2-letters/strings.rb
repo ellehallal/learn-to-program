a1521a689ce767be0ab2c3ffa83663815510fca1
Elle J <itsellej@gmail.com>
2018-03-15 15:59:11 +0000

    Chaper 2 exercise

--- chapter-2-letters/strings.rb
+++ chapter-2-letters/strings.rb
@@ -1,3 +1,18 @@
 puts 'Hello, world!'
 puts ''
 puts 'Good-bye.'
+
+#Now try some strings of your own.
+puts "Hello, my name is Elle"
+puts "Today is Thursday"
+
+puts 'blink ' * 4
+
+puts  2  *  5
+puts '2' *  5
+puts '2  *  5'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down' #backslash doesn't escape d, so the backslash is printed
