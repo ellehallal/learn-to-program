5e4513a743d9c69416ef92b25dd679899a26f67a
Elle J <itsellej@gmail.com>
2018-03-15 16:08:40 +0000

    Chapter 3 exercise

---
+++ chapter-3-variables/variables.rb
@@ -0,0 +1,14 @@
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name + ' is a really long name!'
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
