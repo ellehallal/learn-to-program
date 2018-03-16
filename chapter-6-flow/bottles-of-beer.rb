48ad41ba9a242a897e99caa9c796c513079d96b2
itsellej <itsellej@gmail.com>
2018-03-16 13:43:07 +0000

    Amend bottles of beer

--- chapter-6-flow/bottles-of-beer.rb
+++ chapter-6-flow/bottles-of-beer.rb
@@ -3,20 +3,24 @@
 # "99 Bottles of Beer on the Wall."
 
 def beer(bottles)
-
-    (bottles >= 2 or bottles ==0) ? lyric = "bottles" : lyric = "bottle"
+    while bottles > 0
     
-    new_bottles = bottles - 1
-    new_lyric = ""
-    
-    new_bottles == 1? new_lyric = "bottle" : new_lyric = "bottles"
-    
-    puts "#{bottles} #{lyric} of beer on the wall, #{bottles} #{lyric} of beer.
-    Take one down and pass it around, #{new_bottles} #{new_lyric} of beer on the wall."
+        (bottles >= 2 or bottles ==0) ? lyric = "bottles" : lyric = "bottle"
+        
+        new_bottles = bottles - 1
+        new_lyric = ""
+        
+        new_bottles == 1? new_lyric = "bottle" : new_lyric = "bottles"
+        
+        puts "#{bottles} #{lyric} of beer on the wall, #{bottles} #{lyric} of beer.
+        Take one down and pass it around, #{new_bottles} #{new_lyric} of beer on the wall."
+        
+        bottles -=1
+    end
 end
 
 #Test
-beer(99)
-beer(2)
-beer(1)
-beer(0)
+beer(9999)
+# beer(2)
+# beer(1)
+# beer(0)
