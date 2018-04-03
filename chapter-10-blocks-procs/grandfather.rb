bc4288cb383a526a3c9128eae24e6716c9d7319a
Elle H <itsellej@gmail.com>
2018-04-03 11:02:38 +0100

    Complete grandfather clock

---
+++ chapter-10-blocks-procs/grandfather.rb
@@ -0,0 +1,18 @@
+def clock(my_proc)
+
+  hours = Time.now.hour
+
+  #convert to 12 hour clock
+  if hours == 0
+    hours = 12
+  elsif hours > 12
+    hours -= 12
+  end
+
+  #call block relating to hours passed
+  hours.times{my_proc.call}
+end
+
+chime = Proc.new{puts 'DONG!'}
+
+clock(chime)
