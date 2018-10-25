168fc3f691a8add9d2542b8ae4a0bf9a080ff37f
itsellej <daniellehallal@gmail.com>
2018-10-25 15:52:21 +0100

    Complete custom sort method

--- chapter-7-arrays/exercise-1.rb
+++ chapter-7-arrays/exercise-1.rb
@@ -1,22 +1,30 @@
 # Try writing the program without using the sort method.
+#Note: https://stackoverflow.com/questions/11106981/sorting-an-array-in-ruby-without-using-sort-method
 
-def new_array
-    puts "Enter a word below:"
-    text = gets.chomp
-    my_array = []
+def sorted_array
+  puts "Enter a word below:"
+  text = gets.chomp
+  my_array = []
 
-    while text != ""
+  while text != ""
     my_array << text
     puts "Add another word:"
     text = gets.chomp
-    end
+  end
 
-    sorted = []
-    i = 0
-    while i < my_array.length do
-      #come back to this exercise
-    end
+
+  my_array.length.times.each do |t| #for each item in the array, do the following
+   i = 0
+   my_array.each do |b|
+     if b > my_array[i+1] #if the current item is bigger than the next item
+       my_array[i],my_array[i+1] = my_array[i+1],my_array[i] #swap their positions in the array
+     end
+     i += 1 if i < my_array.size - 2 
+   end
+  end
+
+  my_array
 
 end
 
-new_array
+puts sorted_array
