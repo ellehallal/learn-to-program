3dab8b83d40a02f3882a7b7cfeddf90f11228d0a
itsellej <daniellehallal@gmail.com>
2018-10-25 14:21:13 +0100

    Amend loop

--- chapter-7-arrays/exercise-1.rb
+++ chapter-7-arrays/exercise-1.rb
@@ -1,17 +1,22 @@
-# Try writing the program without using the sort method. 
+# Try writing the program without using the sort method.
 
 def new_array
     puts "Enter a word below:"
     text = gets.chomp
     my_array = []
-    
+
     while text != ""
     my_array << text
     puts "Add another word:"
     text = gets.chomp
     end
-    
-    puts [myarray.first...myarray.last]
+
+    sorted = []
+    i = 0
+    while i < my_array.length do
+      #come back to this exercise
+    end
+
 end
 
 new_array
