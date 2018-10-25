03ff31d74128d8b74c5a72627eab87debd7d9f0c
itsellej <daniellehallal@gmail.com>
2018-10-25 10:26:00 +0100

    Amend formatting

--- chapter-6-flow/bottles-of-beer.rb
+++ chapter-6-flow/bottles-of-beer.rb
@@ -1,29 +1,27 @@
-# "99 bottles of beer on the wall..." Write a program which prints out the 
-# lyrics to that beloved classic, that field-trip favorite: 
+# "99 bottles of beer on the wall..." Write a program which prints out the
+# lyrics to that beloved classic, that field-trip favorite:
 # "99 Bottles of Beer on the Wall."
 
 def beer(bottles)
     while bottles > 0
-    
-        (bottles >= 2 or bottles ==0) ? lyric = "bottles" : lyric = "bottle"
-        
+        (bottles >= 2 or bottles == 0) ? lyric = "bottles" : lyric = "bottle"
+
         new_bottles = bottles - 1
-        new_lyric = ""
-        
-        new_bottles == 1? new_lyric = "bottle" : new_lyric = "bottles"
-        
+
+        new_bottles == 1 ? new_lyric = "bottle" : new_lyric = "bottles"
+
         puts "#{bottles} #{lyric} of beer on the wall, #{bottles} #{lyric} of beer.
         Take one down and pass it around, #{new_bottles} #{new_lyric} of beer on the wall."
-        
+
         bottles -=1
     end
-    
+
     puts "No more bottles of beer on the wall, no more bottles of beer.
     Can't take one down and pass it around, no more bottles of beer on the wall"
 end
 
 #Test
-beer(9999)
+beer(100)
 # beer(2)
 # beer(1)
 # beer(0)
