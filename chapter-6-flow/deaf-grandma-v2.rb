a63856415b4aaeabe2d1294b53976d39a29813bf
itsellej <daniellehallal@gmail.com>
2018-10-25 10:37:38 +0100

    Add array of responses grandma can give when you say BYE

--- chapter-6-flow/deaf-grandma-v2.rb
+++ chapter-6-flow/deaf-grandma-v2.rb
@@ -1,8 +1,8 @@
-# Extend your Deaf Grandma program: What if grandma doesn't want you to leave? 
-# When you shout BYE, she could pretend not to hear you. Change your previous 
-# program so that you have to shout BYE three times in a row. 
+# Extend your Deaf Grandma program: What if grandma doesn't want you to leave?
+# When you shout BYE, she could pretend not to hear you. Change your previous
+# program so that you have to shout BYE three times in a row.
 
-# Make sure to test your program: if you shout BYE three times, but not in a row, 
+# Make sure to test your program: if you shout BYE three times, but not in a row,
 # you should still be talking to grandma.
 
 def deaf_grandma
@@ -10,18 +10,25 @@ def deaf_grandma
     text = gets.chomp
     goodbye = 0
 
-    while goodbye != 2 
+    while goodbye != 2
         if (text == text.upcase and text !='BYE')
             puts "NO, NOT SINCE #{rand(1930..1950)}"
             goodbye = 0
         elsif text == 'BYE'
-            puts "WHAT DID YOU SAY, DEAR?"
+            response = [
+              "WHAT DID YOU SAY, DEAR?",
+              "WHAT DOES THAT MEAN, DEAR?",
+              "OH, THAT'S NICE, DEAR",
+              "IS THAT WHAT YOUR MUM SAID?"
+            ]
+            random_number = rand(1..response.length - 1)
+            puts response[random_number]
             goodbye += 1
         else
             puts "HUH?! SPEAK UP, SONNY!"
             goodbye = 0
         end
-        
+
         text = gets.chomp
     end
     puts "GOODBYE DEAR"
