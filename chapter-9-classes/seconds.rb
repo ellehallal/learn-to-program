2b6eeb292a5ee3aca91d265e0a63d77ace4b94b0
itsellej <daniellehallal@gmail.com>
2018-10-26 10:27:00 +0100

    Add if statement for users who are already over a billion seconds old, and users who are not

--- chapter-9-classes/seconds.rb
+++ chapter-9-classes/seconds.rb
@@ -1,13 +1,32 @@
 #find out when you will turn one billion seconds old
 
-birthdate = Time.new(1986, 5, 25, 16, 04)
-todaydate = Time.now
+birthdate = Time.new(1986, 5, 25)
+birthdate2 = Time.new(1986, 1, 25)
+birthdate3 = Time.new(2000, 1, 25)
 
-secondsage = todaydate - birthdate
-p secondsage.to_i #over 1 billion seconds old
+def billion_seconds_date(birthdate)
+  todaydate = Time.now
 
-# while secondsage > 1000000000
-#   secondsage - (60 * 60 * 24) #take away a day
-# end
+  secondsage = todaydate - birthdate #age in seconds
 
-#[time] - (seconds * minutes * hours * days)
+  datetoday = Time.now
+  oneday = (60 * 60 * 24)
+
+  if secondsage > 1000000000
+    while secondsage > 1000000000
+      datetoday -= oneday #take away a day
+      secondsage -= oneday
+    end
+    "Your one billion seconds birthday was on #{datetoday}"
+  else
+    while secondsage < 1000000000
+      datetoday += oneday #add a day
+      secondsage += oneday
+    end
+    "Your one billion seconds birthday is on #{datetoday}"
+  end
+end
+
+p billion_seconds_date(birthdate)
+p billion_seconds_date(birthdate2)
+p billion_seconds_date(birthdate3)
